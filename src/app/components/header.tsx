d6546998332c176e24ac16128ba188424280947c
Joao Franca <ribeirodefranca23@gmail.com>
2024-04-15 23:42:46 -0300

    fix: correção de pequenos erros

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,9 +13,11 @@ export function Header() {
 
   const handleOpen = () => setIsOpen((prev) => !prev);
   return (
-    <header className="sticky top-0 h-[70px] shadow-xl text-2xl bg-white  ">
-      <div className="flex justify-between h-full items-center mx-12 lg:mx-32">
-        <div className="cursor-pointer">Turismo Facil</div>
+    <header className="sticky top-0 h-[80px] shadow-xl text-2xl bg-white z-20 ">
+      <div className="flex justify-between h-full items-center mx-10 lg:mx-28">
+        <a href="#" className=" hover:text-gray-300 duration-500">
+          Turismo Facil
+        </a>
 
         <nav>
           <div
@@ -24,11 +26,11 @@ export function Header() {
             {isOpen ? <X /> : <List />}
           </div>
           <ul
-            className={`fixed h-0 p-0 
-            flex  flex-col lg:flex-row lg:items-center lg:relative gap-12 lg:p-0 lg:top-0 
-              ease-in left-0 transition-all duration-500 mx-12 lg:mx-16  ${
-                isOpen ? "top-20 opacity-100" : "top-[-490px] "
-              } lg:opacity-100  
+            className={`fixed p-0 h-auto w-full flex flex-col lg:flex-row 
+            lg:items-center lg:relative gap-12 lg:p-0 lg:top-0 
+              ease-in left-0 transition-all duration-500 lg:mx-16  ${
+                isOpen ? "top-20 bg-white p-10" : "top-[-490px] "
+              }   
       `}>
             {Links.map((link: any) => (
               <li key={Links.name} className="">
